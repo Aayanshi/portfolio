51a3acf8e36756b1c1ecd1450417c1687b20378a
Aayanshi Sharma <aayanshi1762@gmail.com>
2026-02-09 18:40:51 +0530

    form updated

--- src/components/ContactDrawer.jsx
+++ src/components/ContactDrawer.jsx
@@ -1,6 +1,38 @@
 import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const ContactDrawer = ({ open, onClose }) => {
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    if (success) {
+      const timer = setTimeout(() => {
+        setSuccess(false);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [success]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // redirect/404 ro
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    try {
+      await fetch("/", {
+        method: "POST",
+        body: formData,
+      });
+
+      setSuccess(true); // success message
+      form.reset(); // fields clear
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <AnimatePresence>
       {open && (
@@ -29,13 +61,17 @@ const ContactDrawer = ({ open, onClose }) => {
           >
             <div className="flex justify-between items-center mb-4 text-purple-900">
               <h2 className="text-xl font-bold">Let’s Connect 👋</h2>
-              <button onClick={onClose} className="text-xl">✖</button>
+              <button onClick={onClose} className="text-xl">
+                ✖
+              </button>
             </div>
 
             <form
               name="contact"
               method="POST"
               data-netlify="true"
+              netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
               className="space-y-4"
             >
               <input type="hidden" name="form-name" value="contact" />
@@ -62,18 +98,23 @@ const ContactDrawer = ({ open, onClose }) => {
                 rows="4"
                 placeholder="Your Message"
                 required
-                className="w-full border px-4 py-2 rounded"
+                className="w-full border px-4 py-2 rounded text-black"
               />
 
               <button
                 type="submit"
-                className="w-full bg-purple-800 text-white py-2 rounded hover:bg-purple-700"
+                disabled={success}
+                className={`w-full py-2 rounded text-white ${
+                  success
+                    ? "bg-gray-400 cursor-not-allowed"
+                    : "bg-purple-800 hover:bg-purple-700"
+                }`}
               >
-                Send Message
+                {success ? "Sent ✅" : "Send Message"}
               </button>
             </form>
             <span className="mt-80 block text-sm text-gray-500 text-center">
-            Made with ❤️ by Aayanshi
+              Made with ❤️ by Aayanshi
             </span>
           </motion.div>
         </motion.div>
